ba7291e34e7fa1dc2a8edb366796932847f271f1
uvsq21915493 <145448904+soufelm78@users.noreply.github.com>
2025-03-11 11:19:30 +0100

    Correction de la compatibilité pour tous les systemes d'exploitations (auparavant windows) avec Prisma

--- server.js
+++ server.js
@@ -67,6 +67,9 @@ app.use(async (req, res, next) => {
 
 app.use(express.json());
 app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "accueil.html"));
+});
 
 // 📌 Middleware pour rediriger les utilisateurs authentifiés
 const redirectIfAuthenticated = (req, res, next) => {
